785dd4e6b487d7e3f5a6f167b9167fe18ff3edae
w44121 <w44121@yandex.ru>
2021-04-16 13:53:03 +0300

    refactor order create service

---
+++ src/orders/order_creater.py
@@ -0,0 +1,25 @@
+from cart.cart import Cart
+from .models import OrderProduct, Order
+from products.models import Product
+
+
+class OrderCreater:
+    def __init__(self, cart, user):
+        self.cart = cart
+        self.order = Order.objects.create(
+            user=user,
+            total_price=self.cart.get_sum()
+        )
+
+    def _get_products_from_cart(self):
+        products = Product.objects.filter(id__in=[x[0] for x in self.cart])
+        return products
+
+    def create_new(self):
+        for product in self._get_products_from_cart():
+            quantity = self.cart.cart[str(product.id)]['quantity']
+            order_project = OrderProduct.objects.create(
+                quantity=quantity,
+                product=product,
+            )
+            self.order.products.add(order_project)
--- src/orders/views.py
+++ src/orders/views.py
@@ -1,34 +1,38 @@
 from rest_framework.response import Response
 from rest_framework import views
+from rest_framework import status
 from .models import Order
 from .serializers import OrderSerializer
-from .order_handler import OrderHandler
+from .order_creater import OrderCreater
+from cart.cart import Cart
 
 
 class OrderView(views.APIView):
     def get(self, request):
         orders = Order.objects.filter(user=request.user, is_deleted=False)
         serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        order = OrderHandler(request).create_new()
-        return Response(order)
+        user = request.user
+        cart = Cart(request)
+        order = OrderCreater(user=user, cart=cart).create_new()
+        return Response(status=status.HTTP_201_CREATED)
 
     def delete(self, request):
         orders = Order.objects.filter(user=request.user)
         for order in orders:
             order.is_deleted = True
             order.save()
-        return Response()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class OrderDetailView(views.APIView):
     def get(self, request, order_id):
         order = Order.objects.get(pk=order_id, is_deleted=False)
         serializer = OrderSerializer(data=order)
-        return Response(data=serializer.data)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, order_id):
         Order.objects.get(pk=order_id).delete()
-        return Response()
+        return Response(status=status.HTTP_204_NO_CONTENT)
