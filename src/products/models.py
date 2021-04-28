ba9ac166b7ac61bc216a4c7971d023ce7c2bc8b0
w44121 <w44121@yandex.ru>
2021-04-28 07:44:36 +0300

    set default image

--- src/products/models.py
+++ src/products/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(default='cpu.jpg')
+    image = models.ImageField(default='no_image.png')  # default image on back?
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     producer = models.ForeignKey(Producer, on_delete=models.CASCADE, related_name='products')
     characteristics = models.JSONField()
