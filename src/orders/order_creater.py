90cf3ac0b4b155a8ca4e730d9bb6fc50afcb7b22
w44121 <w44121@yandex.ru>
2021-04-30 13:11:28 +0300

    add error checking

--- src/orders/order_creater.py
+++ src/orders/order_creater.py
@@ -2,6 +2,10 @@ from .models import OrderProduct, Order
 from products.models import Product
 
 
+class ProductInCartDoesNotExistInDatabase(Exception):
+    pass
+
+
 class OrderCreater:
     def __init__(self, cart, user):
         self.cart = cart
@@ -10,15 +14,21 @@ class OrderCreater:
             total_price=self.cart.get_sum()
         )
 
-    def _get_products_from_cart(self):
+    def _check_products(self, products):
+        return len(self.cart.cart) == len(products)
+
+    def _get_products_from_cart(self) -> list[Product]:
         products = Product.objects.filter(id__in=[x[0] for x in self.cart])
-        return products
+        if self._check_products(products):
+            return products
+        raise ProductInCartDoesNotExistInDatabase
 
     def create_new(self):
         for product in self._get_products_from_cart():
             quantity = self.cart.cart[str(product.id)]['quantity']
-            order_project = OrderProduct.objects.create(
+            order_products = OrderProduct.objects.create(
                 quantity=quantity,
                 product=product,
             )
-            self.order.products.add(order_project)
+            self.order.products.add(order_products)
+        return self.order
