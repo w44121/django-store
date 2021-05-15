e6eb58851d74be79b8914fec4273bd529c9b76ba
w44121 <w44121@yandex.ru>
2021-05-15 15:37:29 +0300

    fix: product model create

--- src/products/signals.py
+++ src/products/signals.py
@@ -12,16 +12,18 @@ def is_product_arrived(product: Product) -> bool:
     """
     Use only with pre_save signal!
     """
-    product_before_save = Product.objects.get(id=product.id)
+    try:
+        product_before_save = Product.objects.get(id=product.id)
+    except Product.DoesNotExist:
+        return False
     return not product_before_save.is_stock and product.is_stock
 
 
 @receiver(pre_save, weak=False, sender=Product)
 def product_pre_save(sender, instance, *args, **kwargs) -> None:
     if is_product_arrived(instance):
-        print('new product')
+
         for subcribe in SubscriptionProductArrival.objects.filter(product=instance):
-            print(subcribe.user.id, instance.id)
             send_product_arrival_notification.apply_async(kwargs={
                 'user_id': subcribe.user.id,
                 'product_id': instance.id,
